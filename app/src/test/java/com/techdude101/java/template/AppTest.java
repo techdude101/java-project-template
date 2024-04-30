bac9278354cefad56723b8df84fab1bed8b25a4c
techdude101 <techdude1011@gmail.com>
2024-04-30 09:25:50 +0100

    Test lint should fail

--- app/src/test/java/com/techdude101/java/template/AppTest.java
+++ app/src/test/java/com/techdude101/java/template/AppTest.java
@@ -1,5 +1,6 @@
 package com.techdude101.java.template;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -14,5 +15,11 @@ public class AppTest {
     //CHECKSTYLE.ON: AbbreviationAsWordInName
     App classUnderTest = new App();
     assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+
+  @Test
+  public void appGreetingIsCorrect() {
+    App classUnderTest = new App();
+    assertEquals("Hello World!", classUnderTest.getGreeting());
   }
 }
