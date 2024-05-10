89408416530cdfbaa5a180869cb73ecf24ce59b9
techdude101 <techdude1011@gmail.com>
2024-05-10 10:40:13 +0100

    Fixed issue reported by linter

--- app/src/test/java/com/techdude101/java/template/AppTest.java
+++ app/src/test/java/com/techdude101/java/template/AppTest.java
@@ -2,6 +2,7 @@ package com.techdude101.java.template;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 /**
